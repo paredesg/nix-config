3f15501480aff864bdcb16113138039cdbdb6873
Guillaume PAREDES <guillaume.paredes@showroomprive.net>
2026-07-15 17:23:14 +0200

    apps

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,10 +1,11 @@
-{ ... }:
+{ config, lib, ... }:
 {
   imports = [
     ./shell/zsh.nix
     ./shell/starship.nix
     ./editors/neovim.nix
     ./apps/git.nix
+    ./apps/bat.nix
   ];
 
   home.stateVersion = "26.05";
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -17,16 +17,18 @@
 
   time.timeZone = "Europe/Paris";
   i18n.defaultLocale = "fr_FR.UTF-8";
-  i18n.extraLocales = {
+  i18n.extraLocaleSettings = {
+  LC_CTYPE = "fr_FR.UTF8";
   LC_ADDRESS = "fr_FR.UTF-8";
-  LC_IDENTIFICATION = "fr_FR.UTF-8";
   LC_MEASUREMENT = "fr_FR.UTF-8";
+  LC_MESSAGES = "fr_FR.UTF-8";
   LC_MONETARY = "fr_FR.UTF-8";
   LC_NAME = "fr_FR.UTF-8";
   LC_NUMERIC = "fr_FR.UTF-8";
   LC_PAPER = "fr_FR.UTF-8";
   LC_TELEPHONE = "fr_FR.UTF-8";
   LC_TIME = "fr_FR.UTF-8";
+  LC_COLLATE = "fr_FR.UTF-8";
   };
   console.keyMap = "fr";
 
