40b9693b2d9503332ddd1e6bbde28b700c07fd76
Guillaume PAREDES <guillaume.paredes@showroomprive.net>
2026-07-15 12:07:40 +0200

    add fonts.nix

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
+
 {
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
+  fonts.packages = with pkgs.nerd-fonts; [
+    fira-code
+    jetbrains-mono
+    meslo-lg
+    hack
+    dejavu-sans-mono
   ];
+
+  fonts.fontconfig.enable = true;
 }
