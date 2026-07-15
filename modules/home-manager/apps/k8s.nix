8b421f219509365291dc34754d7c0ef5665556e4
Guillaume PAREDES <guillaume.paredes@showroomprive.net>
2026-07-15 17:23:22 +0200

    apps

---
+++ modules/home-manager/apps/k8s.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }: {
+  home-manager.users.${config.user} = { config, ... }: {
+    home.packages = with pkgs; [
+      argocd
+      cilium-cli
+      k9s
+      kubectl
+      kubectl-cnpg
+      kubectl-view-secret
+      kubectx
+      kubelogin
+      kubelogin-oidc
+      kubernetes-helm
+      kubeseal
+      kustomize
+      stern
+    ];
+  };
+}
